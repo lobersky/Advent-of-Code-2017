8595fb980d2f29ec437207ae9b6f479956e6a599
lobersky <l.obersky@gmail.com>
2018-04-04 15:02:57 +1000

    Day 4a

---
+++ Day4a/Day4a/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the file content into a string array, each element is one line of file
+            string[] passPhrasesList = System.IO.File.ReadAllLines(@"C:\Users\Liz\Documents\GitHub\Advent-of-Code-2017\Day4a\Day4a\Test.txt");
+            string[] result;
+            string currentWord = "";
+            string[] stringSeparator = new string[] {" "};
+            int validPassPhraseCount = 0;
+            int wordCounter = 0;
+
+            // Investigate the file contents by line
+
+            foreach (string lineOfList in passPhrasesList)
+            {
+                result = lineOfList.Split(stringSeparator, StringSplitOptions.None);
+
+                for (int i = 0; i < result.Length-1; i++)
+                {
+                    currentWord = result[i];
+
+                    for (int j = 0; j < result.Length-1; j++)
+                    {
+                        if ((currentWord != result[j + 1]) && (j + 1 <= result.Length))
+                        {
+                            wordCounter++;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Passphrase is not valid");
+                            break;
+                        }
+                    }
+                    
+                }
+
+                if (wordCounter == result.Length)
+                {
+                    validPassPhraseCount++;
+                }      
+            }
+            
+            Console.WriteLine(validPassPhraseCount.ToString());
+            Console.ReadLine();
+        }
+    }
+}
